780954a1cc5d6689161cc54ffa9d0bb6dc351b15
Francisco Cortez <franciscocortezdev@gmail.com>
2023-01-13 20:51:15 -0600

    User model

---
+++ src/models/user.ts
@@ -0,0 +1,31 @@
+const { DataTypes } = require("sequelize");
+import dbConexion from "../config/dbConexion";
+
+export const User = dbConexion.sequelize.define(
+  "Users",
+  {
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    apellido: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    pass: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    path: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
