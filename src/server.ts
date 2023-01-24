2d3f6ea7f96b6842b5c02e69ed7b0f4549726954
Francisco Cortez <franciscocortezdev@gmail.com>
2023-01-23 20:49:03 -0600

    Server refactoring

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,34 @@
 import express, { Application } from "express"
-import "dotenv/config"
 import cors from "cors"
-import { routeInterface } from "./interfaces/routeInterface"
+import IndexRoutes from './routes/index'
 import dbConexion from "./config/dbConexion"
 
 export class AppServer {
-  PORT = process.env.PORT
-  app: Application = express()
-  authRoutes: routeInterface
+  private PORT: string
+  private app: Application
 
-  constructor(authRoutes: routeInterface){
-    this.authRoutes = authRoutes
-    dbConexion.connect()
-
+  constructor() {
+    this.PORT = process.env.PORT || "3001"
+    this.app = express()
   }
 
   config() {
     this.app.use(express.json())
     this.app.use(cors())
     this.routes()
+    dbConexion.connection()
   }
 
-  start() {
+  async listen() {
     this.config()
     this.app.listen(this.PORT, () => {
-      console.log("Server started")
+      console.log("Server started on port: " + this.PORT)
     })
+
+    
   }
 
   private routes() {
-    this.app.use("/auth", this.authRoutes.getRoutes())
-    
+    this.app.use("/", IndexRoutes.getRoutes())
   }
 }
