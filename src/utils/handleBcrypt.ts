82e0641db6371bc19d47aafaa9e5e4ebc87933ec
Francisco Cortez <franciscocortezdev@gmail.com>
2023-01-25 19:49:38 -0600

    handler bcrypt

--- src/utils/handleBcrypt.ts
+++ src/utils/handleBcrypt.ts
@@ -1,13 +1,11 @@
 import { hash, compare } from "bcrypt"
 
-class hanldeBcrypt {
-  encrypt = (password: string): Promise<string> => {
+export class handleBcrypt {
+  static encrypt = (password: string): Promise<string> => {
     return hash(password, 6)
   }
 
-  decrypt = (password: string, hash: string): Promise<boolean> => {
+  static decrypt = (password: string, hash: string): Promise<boolean> => {
     return compare(password, hash)
   }
 }
-
-export default new hanldeBcrypt()
