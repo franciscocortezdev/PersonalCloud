62f43e438e3f10955bc0344314930c08f7684cb8
Francisco Cortez <franciscocortezdev@gmail.com>
2023-01-26 21:29:16 -0600

    user service

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,17 +1,16 @@
 import { Iuser } from "../interfaces/authInterface"
 import { UserModel } from "../models/user"
 
-class auth{
-  userLogin = async (email: string) => {
-    const responseItem = await UserModel.findOne({  }) 
-    return responseItem
+class authService {
+  FindUser = async (email: string): Promise<Iuser> => {
+    const responseItem = await UserModel.findOne({ where: { email } })
+    return responseItem as unknown as Iuser
   }
-  
-  userRegister = async (user: Iuser) => {
-    const responseInsert = await UserModel.create()
-   responseInsert.set("password", undefined)
+
+  CreateUser = async (user: Iuser) => {
+    const responseInsert = await UserModel.create({ ...user })
     return responseInsert
   }
 }
 
-export default new auth()
+export default new authService()
